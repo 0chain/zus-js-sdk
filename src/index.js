fd2129dd4c766ebf44451b4d6053f7f9943da5e3
raymishtech <raymishtech@gmail.com>
2023-02-12 17:36:51 +0530

    Added sendTransaction method

--- src/index.js
+++ src/index.js
@@ -1,82 +1,17 @@
-import { getConsensusedInformationFromSharders } from "./utils";
+import { getBalanceUtil, submitTransaction } from "./utils";
 import { createWasm } from "./zcn";
 
 let bls;
 let goWasm;
 
 /* tslint:disable:no-console */
-const StorageSmartContractAddress =
-  "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d7";
-const MinerSmartContractAddress =
-  "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d9";
-const InterestPoolSmartContractAddress =
-  "cf8d0df9bd8cc637a4ff4e792ffe3686da6220c45f0e1103baa609f3f1751ef4";
 
-const Endpoints = {
-  REGISTER_CLIENT: "/v1/client/put",
-  PUT_TRANSACTION: "/v1/transaction/put",
-
-  GET_RECENT_FINALIZED: "/v1/block/get/recent_finalized",
-  GET_LATEST_FINALIZED: "/v1/block/get/latest_finalized",
-  GET_CHAIN_STATS: "/v1/chain/get/stats",
-  GET_BLOCK_INFO: "/v1/block/get",
-  CHECK_TRANSACTION_STATUS: "/v1/transaction/get/confirmation",
-  GET_BALANCE: "/v1/client/get/balance",
-  GET_CLIENT: "/v1/client/get",
-  GET_SCSTATE: "/v1/scstate/get",
-
-  // SC REST
-  SC_REST: "/v1/screst/",
-  SC_REST_ALLOCATION: "/v1/screst/" + StorageSmartContractAddress + "/allocation",
-  SC_REST_ALLOCATIONS: "/v1/screst/" + StorageSmartContractAddress + "/allocations",
-  SC_REST_READPOOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getReadPoolStat",
-  SC_REST_WRITEPOOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getWritePoolStat",
-  SC_BLOBBER_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getblobbers",
-  SC_SHARDER_LIST: "/v1/screst/" + MinerSmartContractAddress + "/getSharderList",
-  SC_MINERS_STATS: "/v1/screst/" + MinerSmartContractAddress + "/getMinerList",
-  SC_REST_ALLOCATION_MIN_LOCK: "/v1/screst/" + StorageSmartContractAddress + "/allocation_min_lock",
-
-  GET_LOCKED_TOKENS: "/v1/screst/" + InterestPoolSmartContractAddress + "/getPoolsStats",
-  GET_USER_POOLS: "/v1/screst/" + MinerSmartContractAddress + "/getUserPools",
-
-  // STAKING
-  GET_STORAGESC_POOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getUserStakePoolStat",
-  GET_MINERSC_POOL_STATS: "/v1/screst/" + MinerSmartContractAddress + "/getUserPools",
-  GET_STAKE_POOL_STAT: "/v1/screst/" + StorageSmartContractAddress + "/getStakePoolStat",
-
-  // BLOBBER
-  ALLOCATION_FILE_LIST: "/v1/file/list/",
-  FILE_STATS_ENDPOINT: "/v1/file/stats/",
-  OBJECT_TREE_ENDPOINT: "/v1/file/objecttree/",
-  FILE_META_ENDPOINT: "/v1/file/meta/",
-  RENAME_ENDPOINT: "/v1/file/rename/",
-  COPY_ENDPOINT: "/v1/file/copy/",
-  UPLOAD_ENDPOINT: "/v1/file/upload/",
-  COMMIT_ENDPOINT: "/v1/connection/commit/",
-  // COPY_ENDPOINT: "/v1/file/copy/",
-  // OBJECT_TREE_ENDPOINT: '/v1/file/objecttree/',
-  COMMIT_META_TXN_ENDPOINT: "/v1/file/commitmetatxn/",
-
-  // PROXY
-  PROXY_SERVER_DOWNLOAD_ENDPOINT: "/download",
-
-  // ZEROBOX URLs
-  ZEROBOX_SERVER_GET_MNEMONIC_ENDPOINT: "/v2/getmnemonic",
-  ZEROBOX_SERVER_SHARE_INFO_ENDPOINT: "/shareinfo",
-  ZEROBOX_SERVER_SAVE_MNEMONIC_ENDPOINT: "/v2/savemnemonic",
-  ZEROBOX_SERVER_DELETE_MNEMONIC_ENDPOINT: "/shareinfo",
-  ZEROBOX_SERVER_REFERRALS_INFO_ENDPOINT: "/getreferrals",
-  ZEROBOX_SERVER_FREE_ALLOCATION: "/v2/createallocation",
-  ZEROBOX_SERVER_DELETE_EXIST_ALLOCATION: "/v2/deleteallocation",
-};
-
-const configJson = {
-  miners: [
-    "https://dev.0chain.net/miner02",
-    "https://dev.0chain.net/miner01",
-    "https://dev.0chain.net/miner03",
-  ],
-  sharders: ["https://dev.0chain.net/sharder01", "https://dev.0chain.net/sharder02"],
+const TransactionType = {
+  SEND: 0, // A transaction to send tokens to another account, state is maintained by account
+  DATA: 10, // A transaction to just store a piece of data on the block chain
+  // STORAGE_WRITE : 101, // A transaction to write data to the blobber
+  // STORAGE_READ  : 103,// A transaction to read data from the blobber
+  SMART_CONTRACT: 1000, // A smart contract transaction type
 };
 
 export const init = async (config) => {
@@ -101,23 +36,12 @@ export const init = async (config) => {
 export const Greeter = (name) => `Hello ${name?.toUpperCase()}`;
 
 export const getBalance = async (clientId) => {
-  return new Promise((resolve, reject) => {
-    getConsensusedInformationFromSharders(configJson.sharders, Endpoints.GET_BALANCE, {
-      client_id: clientId,
-    })
-      .then((res) => {
-        resolve(res);
-      })
-      .catch((error) => {
-        if (error?.error === "value not present") {
-          resolve({
-            balance: 0,
-          });
-        } else {
-          reject(error);
-        }
-      });
-  });
+  return getBalanceUtil(clientId);
+};
+
+export const sendTransaction = async (ae, toClientId, val, note) => {
+  console.log("sendTransaction from:", ae, "to:", toClientId, "value:", val, "note:", note);
+  return submitTransaction(ae, toClientId, val, note, TransactionType.SEND);
 };
 
 export const getBalanceWasm = async (clientId) => {
--- src/utils.js
+++ src/utils.js
@@ -23,7 +23,84 @@ var axios = require("axios");
 var BlueBirdPromise  = require("bluebird");
 var moment = require("moment");
 
+const StorageSmartContractAddress =
+  "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d7";
+const MinerSmartContractAddress =
+  "6dba10422e368813802877a85039d3985d96760ed844092319743fb3a76712d9";
+const InterestPoolSmartContractAddress =
+  "cf8d0df9bd8cc637a4ff4e792ffe3686da6220c45f0e1103baa609f3f1751ef4";
+
+const Endpoints = {
+  REGISTER_CLIENT: "/v1/client/put",
+  PUT_TRANSACTION: "/v1/transaction/put",
+
+  GET_RECENT_FINALIZED: "/v1/block/get/recent_finalized",
+  GET_LATEST_FINALIZED: "/v1/block/get/latest_finalized",
+  GET_CHAIN_STATS: "/v1/chain/get/stats",
+  GET_BLOCK_INFO: "/v1/block/get",
+  CHECK_TRANSACTION_STATUS: "/v1/transaction/get/confirmation",
+  GET_BALANCE: "/v1/client/get/balance",
+  GET_CLIENT: "/v1/client/get",
+  GET_SCSTATE: "/v1/scstate/get",
+
+  // SC REST
+  SC_REST: "/v1/screst/",
+  SC_REST_ALLOCATION: "/v1/screst/" + StorageSmartContractAddress + "/allocation",
+  SC_REST_ALLOCATIONS: "/v1/screst/" + StorageSmartContractAddress + "/allocations",
+  SC_REST_READPOOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getReadPoolStat",
+  SC_REST_WRITEPOOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getWritePoolStat",
+  SC_BLOBBER_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getblobbers",
+  SC_SHARDER_LIST: "/v1/screst/" + MinerSmartContractAddress + "/getSharderList",
+  SC_MINERS_STATS: "/v1/screst/" + MinerSmartContractAddress + "/getMinerList",
+  SC_REST_ALLOCATION_MIN_LOCK: "/v1/screst/" + StorageSmartContractAddress + "/allocation_min_lock",
+
+  GET_LOCKED_TOKENS: "/v1/screst/" + InterestPoolSmartContractAddress + "/getPoolsStats",
+  GET_USER_POOLS: "/v1/screst/" + MinerSmartContractAddress + "/getUserPools",
+
+  // STAKING
+  GET_STORAGESC_POOL_STATS: "/v1/screst/" + StorageSmartContractAddress + "/getUserStakePoolStat",
+  GET_MINERSC_POOL_STATS: "/v1/screst/" + MinerSmartContractAddress + "/getUserPools",
+  GET_STAKE_POOL_STAT: "/v1/screst/" + StorageSmartContractAddress + "/getStakePoolStat",
+
+  // BLOBBER
+  ALLOCATION_FILE_LIST: "/v1/file/list/",
+  FILE_STATS_ENDPOINT: "/v1/file/stats/",
+  OBJECT_TREE_ENDPOINT: "/v1/file/objecttree/",
+  FILE_META_ENDPOINT: "/v1/file/meta/",
+  RENAME_ENDPOINT: "/v1/file/rename/",
+  COPY_ENDPOINT: "/v1/file/copy/",
+  UPLOAD_ENDPOINT: "/v1/file/upload/",
+  COMMIT_ENDPOINT: "/v1/connection/commit/",
+  // COPY_ENDPOINT: "/v1/file/copy/",
+  // OBJECT_TREE_ENDPOINT: '/v1/file/objecttree/',
+  COMMIT_META_TXN_ENDPOINT: "/v1/file/commitmetatxn/",
+
+  // PROXY
+  PROXY_SERVER_DOWNLOAD_ENDPOINT: "/download",
+
+  // ZEROBOX URLs
+  ZEROBOX_SERVER_GET_MNEMONIC_ENDPOINT: "/v2/getmnemonic",
+  ZEROBOX_SERVER_SHARE_INFO_ENDPOINT: "/shareinfo",
+  ZEROBOX_SERVER_SAVE_MNEMONIC_ENDPOINT: "/v2/savemnemonic",
+  ZEROBOX_SERVER_DELETE_MNEMONIC_ENDPOINT: "/shareinfo",
+  ZEROBOX_SERVER_REFERRALS_INFO_ENDPOINT: "/getreferrals",
+  ZEROBOX_SERVER_FREE_ALLOCATION: "/v2/createallocation",
+  ZEROBOX_SERVER_DELETE_EXIST_ALLOCATION: "/v2/deleteallocation",
+};
+
+const configJson = {
+  miners: [
+    "https://dev.0chain.net/miner02",
+    "https://dev.0chain.net/miner01",
+    "https://dev.0chain.net/miner03",
+  ],
+  sharders: ["https://dev.0chain.net/sharder01", "https://dev.0chain.net/sharder02"],
+};
+
+
+
 const consensusPercentage = 20;
+let cachedNonce;
 
 // This will return null if not enough consensus , otherwise will return max voted response
 const getConsensusMessageFromResponse = function (hashedResponses, consensusNo, responseArray) {
@@ -394,9 +471,9 @@ export const hexStringToByte = (str) => {
     }
   },
 
-  // export const parseJson = (jsonString) => {
-  //   return JSONbig.parse(jsonString);
-  // }
+  export const parseJson = (jsonString) => {
+    return JSONbig.parse(jsonString);
+  }
 
   export const doParallelPostReqToAllMiners = (miners, url, postData) => {
     return new Promise(function (resolve, reject) {
@@ -441,3 +518,90 @@ export const hexStringToByte = (str) => {
       }
     });
   }
+
+  export const getBalanceUtil = async (clientId) => {
+    return new Promise((resolve, reject) => {
+      getConsensusedInformationFromSharders(configJson.sharders, Endpoints.GET_BALANCE, {
+        client_id: clientId,
+      })
+        .then((res) => {
+          resolve(res);
+        })
+        .catch((error) => {
+          if (error?.error === "value not present") {
+            resolve({
+              balance: 0,
+            });
+          } else {
+            reject(error);
+          }
+        });
+    });
+  };
+
+  export const getTransactionResponse = (data) => {
+    const txn = {};
+    txn.hash = data.hash;
+    txn.version = data.version;
+    txn.client_id = data.client_id;
+    txn.to_client_id = (typeof data.to_client_id != 'undefined') ? data.to_client_id : null;
+    txn.chain_id = data.chain_id;
+    txn.transaction_data = data.transaction_data;
+    txn.transaction_value = data.transaction_value;
+    txn.signature = data.signature;
+    txn.creation_date = data.creation_date;
+    txn.transaction_type = data.transaction_type;
+    txn.transaction_output = data.transaction_output;
+    txn.txn_output_hash = (typeof data.txn_output_hash != 'undefined') ? data.txn_output_hash : null;
+  }
+
+  export const submitTransaction = async (ae, toClientId, val, note, transaction_type) => {
+    const hashPayload = sha3.sha3_256(note);
+    const ts = Math.floor(new Date().getTime() / 1000);
+
+    if (cachedNonce === undefined) {
+        //initialize by 0 if there is no nonce from getBalance as well
+        try {
+            const { nonce } = await getBalanceUtil(ae.id)
+            cachedNonce = nonce ?? 0   
+        }
+        catch (err) {
+           cachedNonce = 0  
+        }
+    }
+
+    const nonceToUse = cachedNonce+1
+    const hashdata = ts + ":" + nonceToUse + ":" + ae.id + ":" + toClientId + ":" + val + ":" + hashPayload;
+    const hash = sha3.sha3_256(hashdata);
+    const bytehash = hexStringToByte(hash);
+    const sec = new bls.SecretKey();
+    sec.deserializeHexStr(ae.secretKey);
+    const sig = sec.sign(bytehash);
+
+    var data = {};
+    data.client_id = ae.id;
+    data.transaction_value = val;
+    data.transaction_data = note;
+    data.transaction_type = transaction_type;
+    data.transaction_nonce = nonceToUse
+    data.creation_date = ts;
+    data.to_client_id = toClientId;
+    data.hash = hash;
+    data.transaction_fee = 0;
+    data.signature = sig.serializeToHexStr();
+    data.version = '1.0';
+    data.txn_output_hash = "";
+    data.public_key = ae.public_key;
+
+    return new Promise(function (resolve, reject) {
+        doParallelPostReqToAllMiners(configJson.miners, Endpoints.PUT_TRANSACTION, data)
+            .then((response) => {
+                cachedNonce +=1
+
+                resolve(getTransactionResponse(response.entity));
+            })
+            .catch((error) => {
+                reject(error);
+            })
+    });
+}
